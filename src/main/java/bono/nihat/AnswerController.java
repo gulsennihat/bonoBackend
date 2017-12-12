09d648234523b04d5d3698ef449809a18cc780ae
Nihat <nihat@Nihat-MacBook-Pro.local>
2017-12-12 08:28:39 +0300

    properties file eklendi

--- src/main/java/bono/nihat/AnswerController.java
+++ src/main/java/bono/nihat/AnswerController.java
@@ -1,5 +1,11 @@
 package bono.nihat;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,9 +14,38 @@ public class AnswerController {
 
 
     @RequestMapping("/answer")
-    public Answer greeting() {
+    public Answer greeting() throws Exception {
     	Answer answer = new Answer();
-    	answer.setAnswer("Her zaman imkansizi hayal ediyorsun.");
+    	Map<Integer, String> answerMap = new HashMap();
+    	answerMap = readPropertiesFileAsMap("./resources/answers.properties", "-");
+    	
+    	Random rand = new Random(); 
+    	Integer rnd = rand.nextInt(answerMap.size()); 
+
+    	answer.setAnswer(answerMap.get(rnd));
         return answer;
     }
+    
+    public static Map<Integer, String> readPropertiesFileAsMap(String filename, String delimiter)
+    		throws Exception
+    		{
+    		  Map<Integer, String> map = new HashMap();
+    		  BufferedReader reader = new BufferedReader(new FileReader(filename));
+    		  String line;
+    		  int key = 0;
+    		  while ((line = reader.readLine()) != null)
+    		  {
+//    		    if (line.trim().length()==0) continue;
+//    		    if (line.charAt(0)=='#') continue;
+//    		    // assumption here is that proper lines are like "String : http://xxx.yyy.zzz/foo/bar",
+//    		    // and the ":" is the delimiter
+//    		    int delimPosition = line.indexOf(delimiter);
+//    		    String key = line.substring(0, delimPosition-1).trim();
+//    		    String value = line.substring(delimPosition+1).trim();
+    		    map.put(key, line);
+    		    key++;
+    		  }
+    		  reader.close();
+    		  return map;
+    		}
 }
